abe3261f6f108bbc0415d0b660a039485dc0ec20
simileexpert <simileoluwa.babalola@outlook.com>
2024-07-10 19:42:10 +0100

    dbcontext

---
+++ OrangeLakeAPI/Controllers/RegionsController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using OrangeLakeAPI.Data;
+
+namespace OrangeLakeAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RegionsController : ControllerBase
+    {
+        private readonly OrangeLakeDbContext dbContext;
+
+        public RegionsController(OrangeLakeDbContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            var regions = dbContext.Regions.ToList();
+
+            return Ok(regions);
+
+        }
+
+
+        //GET REGION BY ID
+        [HttpGet]
+        [Route("{id:Guid}")]
+        public IActionResult GetAllById([FromRoute] Guid id)
+        {
+            var regions = dbContext.Regions.FirstOrDefault(x => x.Id == id);
+
+            if (regions == null)
+            {
+                return NotFound();
+            }
+            return Ok(regions);
+        }
+    }
+}
